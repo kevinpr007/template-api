e5858d31962eb181002c3bcb0e40b538f92eb916
Kevin Rivera <kevinpr_007@hotmail.com>
2018-03-18 00:44:58 -0400

    Add logger to file feature

---
+++ src/config/logger.js
@@ -0,0 +1,47 @@
+import bunyan from 'bunyan';
+import moment from 'moment';
+
+const reqSerializer = req => {
+    return {
+        method: req.method,
+        url: req.url,
+        headers: req.headers
+    };
+}
+
+const dbSerializer = data => {
+    const query = JSON.stringify(data.query)
+    const options = JSON.stringify(data.options || {})
+  
+    return `db.${data.coll}.${data.method}(${query}, ${options})`
+  }
+  
+export default bunyan.createLogger({
+    name: `Template-API`,
+    src: false,
+    streams: [
+        {
+          level: 'debug',
+          stream: process.stdout     
+        },
+        {
+            type: 'rotating-file',
+            level: 'debug', //TODO: Change log levels 
+            path: `./log/${moment(new Date()).format('YYYY-MM-DD')}.log`,
+            period: '1d',   // daily rotation 
+            count: 5        // keep 5 back copies 
+        }
+      ],
+      serializers: {
+        // Serializers
+        // req: reqSerializer,
+        dbQuery: dbSerializer
+
+        // req: bunyan.stdSerializers.req,
+        // res: bunyan.stdSerializers.res,
+        
+        // Examples 
+        // logger.debug({req: req})
+        // logger.debug({res: res})
+      }
+})
--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -1,38 +1,43 @@
+import logger from './logger';
 import mongoose from 'mongoose';
 import Promise from "bluebird";
 mongoose.Promise = Promise;
 
 export default () => {
-        mongoose.set('debug', (coll, method, query, doc, options) => {
-        const set = {
-          coll: coll,
-          method: method,
-          query: query,
-          doc: doc,
-          options: options
-        }
-    
-        console.log({
-          dbQuery: set
-        })
-      })
-    
-      mongoose.connection.on('connecting', () => {
-        console.log('Trying to establish a connection to MongoDB')
-      })
-    
-      mongoose.connection.on('connected', () => {
-        console.log('Connection established successfully')
-      })
-    
-      mongoose.connection.on('error', err => {
-        console.log(`Connection to MongoDB failed: ${err}`)
-      })
-    
-      mongoose.connection.on('disconnected', () => {
-        console.log('MongoDB connection closed')
-      })
 
-      return mongoose.connect(process.env.MONGODB_URL);
+  //TODO: Change debug levels
+  mongoose.set('debug', true);
+
+  mongoose.set('debug', (coll, method, query, doc, options) => {
+    const set = {
+      coll: coll,
+      method: method,
+      query: query,
+      doc: doc,
+      options: options
     }
+
+    logger.debug({
+      dbQuery: set
+    })
+  })
+
+mongoose.connection.on('connecting', () => {
+  console.log('Trying to establish a connection to MongoDB')
+})
+
+mongoose.connection.on('connected', () => {
+  console.log('Connection established successfully')
+})
+
+mongoose.connection.on('error', err => {
+  console.log(`Connection to MongoDB failed: ${err}`)
+})
+
+mongoose.connection.on('disconnected', () => {
+  console.log('MongoDB connection closed')
+})
+
+  return mongoose.connect(process.env.MONGODB_URL);
+}
     
