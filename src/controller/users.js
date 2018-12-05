af00926b57cc4b6ebf7e7613d3dc4505f11e16df
Kevin Rivera <kevinpr_007@hotmail.com>
2018-12-05 16:23:11 -0400

    Add controller to User Route

---
+++ src/controller/users.js
@@ -0,0 +1,50 @@
+const HttpStatus = require('http-status-codes')
+const User = require('../models/User')
+const parseErrors = require('../utils/parseErrors')
+const globalError = require('../utils/globalError')
+const { sendConfirmationEmailValidation } = require('../utils/email/mailer')
+const userFactory = require('../utils/userFactory')
+
+const signUp = (req, res) => {
+	const { email, password, username } = req.body.user
+
+	const user = new User({ email, username })
+	if (user.isPasswordLength(password)) {
+		user.setPassword(password)
+		user.setConfirmationToken()
+		//TODO:ASYNC AWAY
+		user
+			.save()
+			.then((userRecord) => {
+				sendConfirmationEmailValidation(userRecord)
+				res.json({ user: userRecord.toAuthJSON() })
+			})
+			.catch((err) =>
+				res
+					.status(HttpStatus.BAD_REQUEST)
+					.json(globalError('Error saving User', parseErrors(err.errors)))
+			)
+	} else {
+		res
+			.status(HttpStatus.BAD_REQUEST)
+			.json(
+				globalError(
+					`You have entered less than ${
+						process.env.PASSWORD_LENGTH
+					} characters for password`
+				)
+			)
+	}
+}
+
+const currentUser = (req, res) => {
+    //TODO: Add if it's valid
+	res.json({
+		user: userFactory(req.currentUser),
+	})
+}
+
+module.exports = {
+	signUp,
+	currentUser,
+}
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,52 +1,10 @@
 const express = require('express')
-const HttpStatus = require('http-status-codes')
-const User = require('../models/User')
-const parseErrors = require('../utils/parseErrors')
-const globalError = require('../utils/globalError')
-const { sendConfirmationEmailValidation } = require('../utils/email/mailer')
 const authenticate = require('../middlewares/authenticate')
-const userFactory = require('../utils/userFactory')
-
+const { signUp, currentUser } = require('../controller/users')
 const router = express.Router()
 
-//TODO:Add Controllers
 //TODO:Add Sign Up
-router.post('/', (req, res) => {
-	const { email, password, username } = req.body.user
-
-	const user = new User({ email, username })
-	if (user.isPasswordLength(password)) {
-		user.setPassword(password)
-		user.setConfirmationToken()
-		//TODO:ASYNC AWAY
-		user
-			.save()
-			.then((userRecord) => {
-				sendConfirmationEmailValidation(userRecord)
-				res.json({ user: userRecord.toAuthJSON() })
-			})
-			.catch((err) =>
-				res
-					.status(HttpStatus.BAD_REQUEST)
-					.json(globalError('Error saving User', parseErrors(err.errors)))
-			)
-	} else {
-		res
-			.status(HttpStatus.BAD_REQUEST)
-			.json(
-				globalError(
-					`You have entered less than ${
-						process.env.PASSWORD_LENGTH
-					} characters for password`
-				)
-			)
-	}
-})
-
-router.get('/current_user', authenticate, (req, res) => {
-	res.json({
-		user: userFactory(req.currentUser),
-	})
-})
+router.post('/', signUp)
+router.get('/current_user', authenticate, currentUser)
 
 module.exports = router
