fd77965806145c2fe798b12d53476dbd23db0b48
Kevin Rivera <kevinpr_007@hotmail.com>
2018-12-10 16:19:40 -0400

    Fix routes and apply middleware

--- src/routes/entity1.js
+++ src/routes/entity1.js
@@ -9,11 +9,12 @@ const {
 } = require('../controllers/entity1')
 const router = express.Router()
 
-//TODO: Authenticate Use
-router.get('/', authenticate, getAll)
-router.post('/', authenticate, insert)
-router.get('/:id', authenticate, getById)
-router.put('/:id', authenticate, updateById)
-router.delete('/:id', authenticate, deleteById)
+router.use(authenticate)
+
+router.get('/', getAll)
+router.post('/', insert)
+router.get('/:id', getById)
+router.put('/:id', updateById)
+router.delete('/:id', deleteById)
 
 module.exports = router
