87ab1ec040cfad5068a73fa1c2261a0901e64499
Kevin Rivera <kevinpr_007@hotmail.com>
2018-12-19 15:51:52 -0400

    Add validation for default variables

--- src/middlewares/defaultVariables.js
+++ src/middlewares/defaultVariables.js
@@ -1,5 +1,14 @@
 module.exports = (req, res, next) => {
-	req.query.page = parseInt(req.query.page) || 1
+	req.query.page = parseInt(req.query.page || 1)
 	req.query.limit = parseInt(process.env.REST_Limit_Page)
+
+	if (req.query.page === undefined || isNaN(req.query.page)) {
+		let message = new Error('You must provide a valid page number')
+		next(message)
+	}
+	// if (req.query.pageSize === undefined || isNaN(req.query.pageSize)) {
+	// let message = 'You must provide a valid Page size number'
+	// // return util.generateError(message, HttpStatus.BAD_REQUEST)
+	// }
 	next()
 }
