b848e0fbe18ced60a1c9170ce871e88b2ea83792
Kevin Rivera <kevinpr_007@hotmail.com>
2018-12-20 10:57:33 -0400

    Add Pre-Hooks Feature

---
+++ src/models/plugins/hooks.js
@@ -0,0 +1,32 @@
+module.exports = (schema, options) => {
+	// Create a pre-save hook
+	schema.pre('save', function(next) {
+		// Set a values
+		if (this.__v !== undefined) {
+			this.__v = this.__v + 1
+		}
+
+		// Call the next function in the pre-save chain
+		next()
+	})
+
+	schema.pre('findOneAndUpdate', function(next) {
+		// Set a values
+		if (this.__v !== undefined) {
+			this.__v = this.__v + 1
+		}
+
+		// Call the next function in the pre-save chain
+		next()
+	})
+
+	schema.pre('findByIdAndUpdate', function(next) {
+		// Set a values
+		if (this.__v !== undefined) {
+			this.__v = this.__v + 1
+		}
+
+		// Call the next function in the pre-save chain
+		next()
+	})
+}
