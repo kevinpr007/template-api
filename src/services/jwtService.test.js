1cec99eb95d709f3c11fbc9cdef4f6264ff150cd
Kevin Rivera <kevinpr_007@hotmail.com>
2019-01-03 11:56:23 -0400

    Add jwtService Test

---
+++ src/services/jwtService.test.js
@@ -0,0 +1,64 @@
+const jwt = require('jsonwebtoken')
+const jwtService = require('./jwtService')
+
+describe('jwtService.js', () => {
+	describe('Test jwt service functions', () => {
+		beforeEach(() => {
+			jest.resetModules()
+		})
+
+		test('sign', async () => {
+			//Setting
+			expect.assertions(2)
+
+			jwt.sign = jest.fn(() => 'Token-12345')
+			jwtService.sign('user')
+
+			//Must be called once
+			expect(jwt.sign).toHaveBeenCalledTimes(1)
+
+			//Required variable can't be null
+			try {
+				jwtService.sign(null)
+			} catch (err) {
+				expect(err).toBeInstanceOf(Error)
+			}
+		})
+
+		test('ResetPasswordSign', async () => {
+			//Setting
+			expect.assertions(2)
+
+			jwt.sign = jest.fn(() => 'Token-12345')
+			jwtService.ResetPasswordSign('user')
+
+			//Must be called once
+			expect(jwt.sign).toHaveBeenCalledTimes(1)
+
+			//Required variable can't be null
+			try {
+				jwtService.ResetPasswordSign(null)
+			} catch (err) {
+				expect(err).toBeInstanceOf(Error)
+			}
+		})
+
+		test('verify', async () => {
+			//Setting
+			expect.assertions(2)
+
+			jwt.verify = jest.fn(() => 'Token-12345')
+			jwtService.verify('user')
+
+			//Must be called once
+			expect(jwt.verify).toHaveBeenCalledTimes(1)
+
+			//Required variable can't be null
+			try {
+				jwtService.verify(null)
+			} catch (err) {
+				expect(err).toBeInstanceOf(Error)
+			}
+		})
+	})
+})
