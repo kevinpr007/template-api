31db6996154e38a5ee6275916cb720601954aa40
Kevin Rivera <kevinpr_007@hotmail.com>
2019-01-04 15:16:46 -0400

    Add authorize test

---
+++ src/middlewares/authorize.test.js
@@ -0,0 +1,37 @@
+const authorize = require('./authorize')
+
+describe('authorize.js', () => {
+	describe('Test authorize function', () => {
+		beforeEach(() => {
+			jest.resetModules()
+		})
+
+		test('authorize', async () => {
+			//Setting
+			expect.assertions(2)
+
+			let roles = 'Admin'
+			let req = {
+				currentUser: {
+					roles: ['Admin'],
+				},
+			}
+			let res
+			let next = jest.fn()
+
+			//Check Response
+			let response = authorize(roles)
+			response[0](req, res, next)
+			expect(next).toHaveBeenCalledTimes(1)
+
+			req.currentUser.roles = 'OTHER_ROLE'
+			res = {
+				status: jest.fn().mockReturnValue({
+					json: jest.fn(),
+				}),
+			}
+			response[0](req, res, next)
+			expect(res.status().json).toHaveBeenCalledTimes(1)
+		})
+	})
+})
