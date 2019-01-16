2f8c4115032d99b5231b840c0430cb24e4b7909b
Kevin Rivera <kevinpr_007@hotmail.com>
2019-01-16 10:42:33 -0400

    Add entity2 and user seeds

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -1,17 +1,54 @@
 const HttpStatus = require('http-status-codes')
 const faker = require('faker')
 const Entity1 = require('../models/entity1')
+const Entity2 = require('../models/entity2')
+const User = require('../models/user')
 
 const addSeed = async (req, res, next) => {
 	try {
+		let toPushEntity1 = []
+
 		for (let i = 0; i < 100; i++) {
 			let entity = new Entity1({
 				MyField: faker.name.findName(),
 				MyDescription: faker.lorem.sentences(),
 				MyNumberField: faker.random.number(),
 			})
-			await entity.save()
+			toPushEntity1.push(entity)
 		}
+		Entity1.collection.insert(toPushEntity1, (err, docs) => {
+			console.log(docs)
+		})
+
+		let toPushEntity2 = []
+
+		for (let i = 0; i < 100; i++) {
+			let entity = new Entity2({
+				MyString: faker.lorem.sentences(),
+				MyNumber: faker.random.number(),
+				MyDescription: faker.lorem.sentences(),
+			})
+			toPushEntity2.push(entity)
+		}
+		Entity2.collection.insert(toPushEntity2, (err, docs) => {
+			console.log(docs)
+		})
+
+		let toPushUser = []
+
+		for (let i = 0; i < 15; i++) {
+			let entity = new User({
+				email: faker.internet.email(),
+				username: faker.internet.userName(),
+				roles: faker.random.arrayElement(['User', 'Admin']),
+				passwordHash: faker.internet.password(),
+				confirmed: faker.random.boolean(),
+			})
+			toPushUser.push(entity)
+		}
+		User.collection.insert(toPushUser, (err, docs) => {
+			console.log(docs)
+		})
 		res.status(HttpStatus.OK).json()
 	} catch (err) {
 		next(err)
