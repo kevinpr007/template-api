b40ba8e216758a0c3c074b0116956162ce59fb52
Kevin Rivera <kevinpr_007@hotmail.com>
2019-01-16 16:11:33 -0400

    Add documentation for Admin Routes

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -11,6 +11,29 @@ router.use(authenticate)
 router.use(refreshTokenJWT)
 router.use(authorize(ADMIN))
 
+/**
+ * @api {get} /admin/addSeed Add Seed
+ * @apiName addSeed
+ * @apiGroup Admin
+ * @apiVersion 1.0.0
+ *
+ * @apiDescription This route will create all entities information on the system.
+ *
+ * @apiPermission Admin
+ *
+ * @apiHeader {String} Content-Type JSON Format.
+ * @apiHeader {String} Authorization Token created by the system.
+ *
+ * @apiHeaderExample {json} Header-Example:
+ *     {
+ *          "Content-Type": "application/json",
+ *          "Authorization": "Bearer {TOKEN-XXXXXXX}"
+ *     }
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *
+ */
 router.get('/addSeed', addSeed)
 
 module.exports = router
