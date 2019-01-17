04213791302903e305376afb5a89750e5e28ee17
Kevin Rivera <kevinpr_007@hotmail.com>
2019-01-17 10:11:42 -0400

    Move landing page

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -2,7 +2,12 @@ const path = require('path')
 
 const MainPage = async (req, res, next) => {
 	try {
-		res.sendFile(path.join(__dirname, '../../public/index.html'))
+		res.sendFile(path.join(__dirname, '../../public/mainpage/index.html'))
+	} catch (err) {
+		next(err)
+	}
+}
+
 	} catch (err) {
 		next(err)
 	}
